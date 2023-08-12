bc889ca5a586fcf777f57c44093740bdae595426
MelinaPl <melina.plakidis@gmx.de>
2023-08-12 14:06:53 +0200

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -8,15 +8,17 @@ from sklearn.metrics import classification_report
 
 from datasets import Dataset
 
-
+#### Set device
 device = torch.device("mps")
 
+#### Set paths
 WORKING_DIR = os.path.dirname(__file__)
 DATA_DIR = "data"
 MODEL_DIR = os.path.join(WORKING_DIR, "models")
 full_path = os.path.join(WORKING_DIR, DATA_DIR)
 tokenizer = AutoTokenizer.from_pretrained("dbmdz/bert-base-german-cased")
 
+#### Define methods for preprocessing and evaluation metrics
 def preprocess_function(examples):
     text = examples["texts"]
     return tokenizer(text, padding=True, truncation=True, return_tensors="pt").to(device)
@@ -29,14 +31,14 @@ def compute_metrics(eval_preds):
     print(df_report.to_latex())
     return report
 
-
+#### Create label mappings (comment out the ones that are not needed)
 # labels_to_hs_ids = {'offensive': 0, 'other': 1}
 # ids_to_hs_labels = {0: 'offensive', 1: 'other'}
 labels_to_hs_ids = {'abuse': 0, 'explicit': 1, 'implicit': 2, 'insult': 3, 'other': 4, 'profanity': 5}
 ids_to_hs_labels = {0: 'abuse', 1: 'explicit', 2: 'implicit', 3: 'insult', 4: 'other', 5: 'profanity'}
 
 
-
+#### Read in data
 test = pd.read_csv(os.path.join(full_path,"multi_test_v1_dataset.csv"))
 train = pd.read_csv(os.path.join(full_path,"multi_train_v1_dataset.csv"))
 train_dataset = Dataset.from_pandas(train)
@@ -44,7 +46,7 @@ test_dataset = Dataset.from_pandas(test)
 tokenized_data_test = test_dataset.map(preprocess_function, batched=True, remove_columns="texts")
 tokenized_data_train = train_dataset.map(preprocess_function, batched=True, remove_columns="texts")
 
-
+#### Load model and show a test prediction
 model = AutoModelForSequenceClassification.from_pretrained(os.path.join(MODEL_DIR, "model_cmv1/checkpoint-300/"), local_files_only=True)
 data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
 input_text = "@SPIEGEL_Politik[SEP]Warum veröffentlicht ihr keine Bilder von linken Anarchisten?[SEP]Die gibt es zuhauf![SEP]Aber ihr seit nur ein Unterstützer dieser Horden![SEP]DIRECTIVE[SEP]DIRECTIVE[SEP]ASSERTIVE[SEP]ASSERTIVE"
@@ -56,16 +58,21 @@ print(model.config.id2label[predicted_class_id])
 
 
 training_args = TrainingArguments(
-    output_dir="evaluation",
-    learning_rate=1e-5,
-    optim="adamw_hf",
+    output_dir=os.path.join(MODEL_DIR, "model_cmv1"),
+    learning_rate=2e-5,
+    optim="adamw_torch", 
     auto_find_batch_size=True,
-    num_train_epochs=6,
+    save_total_limit=1,
+    num_train_epochs=10,
+    warmup_ratio=0.1,
     weight_decay=0.01,
     evaluation_strategy="epoch",
     save_strategy="epoch",
     load_best_model_at_end=True,
-    metric_for_best_model="accuracy",
+    metric_for_best_model="weighted_f1",
+    data_seed=123,
+    seed=123,
+    full_determinism=True,
     use_mps_device=True
 )
 
@@ -79,4 +86,5 @@ trainer = Trainer(
     compute_metrics=compute_metrics
 )
 
+#### Evaluate
 print(trainer.evaluate())
